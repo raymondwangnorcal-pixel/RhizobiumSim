10c2376937b883c4838a469dc232ef7f58c54ff8
Raymond Wang <raymondwang@Raymonds-MacBook-Air-326.local>
2026-04-27 20:49:04 -0400

    Print helper method

--- SimModel.py
+++ SimModel.py
@@ -1,6 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def print_matrix(matrix, name):
+    print(name)
+    print("Rows: Lux, Marshall")
+    print("Columns: Lux plant population, Marshall plant population")
+    print(np.round(matrix, 3))
+    print()
+
 def main():
     high_nitrogen_matrix = np.array([
         [0.230, 0.237],
@@ -12,11 +19,8 @@ def main():
         [0.192, 0.234]
     ])
 
-    print("High Nitrogen Matrix:")
-    print(high_nitrogen_matrix)
-
-    print("Low Nitrogen Matrix:")
-    print(low_nitrogen_matrix)
+    print_matrix(high_nitrogen_matrix, "High Nitrogen Payoff Matrix:")
+    print_matrix(low_nitrogen_matrix, "Low Nitrogen Payoff Matrix:")
 
 if __name__ == "__main__":
     main()
