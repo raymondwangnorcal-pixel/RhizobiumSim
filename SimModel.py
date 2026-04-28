270850f81e1676a8230f1ff0901d0dfd7e3f57ca
Raymond Wang <raymondwang@Raymonds-MacBook-Air-326.local>
2026-04-27 21:05:34 -0400

    Nash Equilibrium check

--- SimModel.py
+++ SimModel.py
@@ -8,6 +8,17 @@ def print_matrix(matrix, name):
     print(np.round(matrix, 3))
     print()
 
+def nash_equilibrium(matrix):
+    lux_payoffs = matrix[0]
+    marshall_payoffs = matrix[1]
+
+    if np.all(lux_payoffs > marshall_payoffs):
+        return "Lux is the dominant strategy. Nash equilibrium: Lux dominance."
+    elif np.all(marshall_payoffs > lux_payoffs):
+        return "Marshall is the dominant strategy. Nash equilibrium: Marshall dominance."
+    else:
+        return "There is no simple dominant strategy."
+
 def main():
     high_nitrogen_matrix = np.array([
         [0.230, 0.237],
@@ -20,7 +31,11 @@ def main():
     ])
 
     print_matrix(high_nitrogen_matrix, "High Nitrogen Payoff Matrix:")
+    print(nash_equilibrium(high_nitrogen_matrix))
+    print()
+
     print_matrix(low_nitrogen_matrix, "Low Nitrogen Payoff Matrix:")
+    print(nash_equilibrium(low_nitrogen_matrix))
 
 if __name__ == "__main__":
     main()
